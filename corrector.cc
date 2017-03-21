b059dffc47e8ff7de526ccd89b01580445b29e84
Grzegorz Pawelczak <gp13823@my.bristol.ac.uk>
2017-03-21 22:01:13 +0000

    more indentation fixes

--- corrector.cc
+++ corrector.cc
@@ -6,19 +6,19 @@
 int main(int argc, char *argv[])
 {
 
-	if(argc != 2)
-	{
-		std::cerr << "Specifiy file name." << std::endl;
-		return -1;
-	}
+  if(argc != 2)
+  {
+    std::cerr << "Specifiy file name." << std::endl;
+    return -1;
+  }
 
 
-	std::vector<ASM_Function> functions = SECDED_for_text::generate_secded_for_text(argv[1]);
-	// SECDED_for_text::print_text_and_secded(&instructions);
+  std::vector<ASM_Function> functions = SECDED_for_text::generate_secded_for_text(argv[1]);
+  // SECDED_for_text::print_text_and_secded(&instructions);
 
-	fault_injector::inject_faults(1, 2, &functions);
-	uint64_t faulty_instructions = SECDED_for_text::check_secded(&functions);
-	std::cerr << "There are " << faulty_instructions << " faulty instructions." << std::endl;
+  fault_injector::inject_faults(1, 2, &functions);
+  uint64_t faulty_instructions = SECDED_for_text::check_secded(&functions);
+  std::cerr << "There are " << faulty_instructions << " faulty instructions." << std::endl;
 
-	return 0;
+  return 0;
 }
--- sample.c
+++ sample.c
@@ -7,14 +7,14 @@ uint32_t calc(uint32_t a, uint32_t b) {return a / b;}
 
 int main()
 {
-	uint32_t a = 20;
-	uint32_t b = 2;
+  uint32_t a = 20;
+  uint32_t b = 2;
 
-	uint32_t c = a + b;
-	uint32_t d = a - b;
+  uint32_t c = a + b;
+  uint32_t d = a - b;
 
-	printf("%u %u %u\n", c, d, calc(a,b));
+  printf("%u %u %u\n", c, d, calc(a,b));
 
-	return 0;
+  return 0;
 
 }
